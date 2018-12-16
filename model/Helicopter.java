016111429171f40222c20aa3477a04c2e245084f
Mohamad <m.mahdavi79@gmail.com>
2018-12-16 12:57:07 +0330

    fix typo

--- model/Helicopter.java
+++ model/Helicopter.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
-public class Hellicopter extends Vehicle {
-    public Hellicopter() {
+public class Helicopter extends Vehicle {
+    public Helicopter() {
         //TODO actual numbers
         setCapacity(5);
         setCurrentCapacity(5);
