7a93d9bf5527b85f73dbf0e4c5964049e821074b
Mohamad <m.mahdavi79@gmail.com>
2019-01-31 22:33:06 +0330

    fix host joining

--- network/TeamGame.java
+++ network/TeamGame.java
@@ -1,22 +1,17 @@
 import java.util.ArrayList;
 
 public class TeamGame {
-    String p1, p2;
-    ArrayList<String> goals = new ArrayList<>();
+    private String player1, player2;
+    private ArrayList<String> goals = new ArrayList<>();
 
-    public TeamGame(String p1, String p2, ArrayList<String> goals) {
-        this.p1 = p1;
-        this.p2 = p2;
+    public TeamGame(String player1, String player2, ArrayList<String> goals) {
+        this.player1 = player1;
+        this.player2 = player2;
         this.goals = goals;
     }
 
     public void remove(String goal) {
-        for (String item : goals) {
-            if(item.equals(goal)) {
-                goals.remove(goal);
-                return ;
-            }
-        }
+        goals.remove(goal);
     }
 
     public ArrayList<String> getGoals() {return goals;}
@@ -24,8 +19,28 @@ public class TeamGame {
     @Override
     public boolean equals(Object obj) {
         if(!(obj instanceof TeamGame)) return false;
-        if(((TeamGame) obj).p1.equals(p1) && ((TeamGame) obj).p2.equals(p2)) return true;
-        if(((TeamGame) obj).p1.equals(p2) && ((TeamGame) obj).p2.equals(p1)) return true;
+        if(((TeamGame) obj).player1.equals(player1) && ((TeamGame) obj).player2.equals(player2)) return true;
+        if(((TeamGame) obj).player1.equals(player2) && ((TeamGame) obj).player2.equals(player1)) return true;
         return false;
+    }
+
+    public String getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(String player1) {
+        this.player1 = player1;
+    }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(String player2) {
+        this.player2 = player2;
+    }
+
+    public void setGoals(ArrayList<String> goals) {
+        this.goals = goals;
     }
 }
