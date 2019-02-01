905ea163c69b609a0396107be9d3e17581532022
SmsS4 <77867908g>
2019-02-01 23:10:09 +0330

    fixed bug

--- view/MultiPlayerMenu.java
+++ view/MultiPlayerMenu.java
@@ -103,7 +103,7 @@ public class MultiPlayerMenu {
         Label logOut = new Label("LOGOUT");
         logOut.setId("label_button");
         logOut.setOnMouseClicked(event -> {
-            client.closلeSocket();
+            client.closeSocket();
             view.setRoot(new Menu(view).getRoot());
         });
         return logOut;
